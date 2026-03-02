274b4f9ae8cbd70a8b7e2340bc694e86f2589c18
Amr Turki <amrturki79@gmail.com>
2026-03-02 12:51:07 +0200

    Initial commit - Business-Card

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BusinessCard());
+}
+
+class BusinessCard extends StatelessWidget {
+  const BusinessCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+  }
+}
